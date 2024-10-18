a10ee32ff106a7648eaf2786aeba8645c2c5cf39
git <florah@gmail.com>
2024-10-18 12:14:12 +0300

    correct errors

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Search from "../../Components/Search/Search";
-import Results from "../../Components/Results/Results";
+import Search from '../../Components/Search/search';
+import Results from "../../Components/Results/results";
 import "./Home.css";
 
 function Home() {
